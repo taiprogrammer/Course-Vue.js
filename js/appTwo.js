2c54a71c99456517c5581da5b94d645a9114cb62
taiprogrammer <taiza.marques@bandtec.com.br>
2021-12-10 12:39:10 -0300

    learning about v-html

--- js/appTwo.js
+++ js/appTwo.js
@@ -1,16 +1,9 @@
-// items: [
-            //     { courseGoal: 'Finish the course and learn Vue!' },
-            //     { courseGoal: 'Finish the course of Kotlin!' },
-            //     { courseGoal: 'Finish the course of Modern Web!' },
-            //     { courseGoal: 'Finish the course of React!' },
-            //     { courseGoal: 'Finish the course of React Native!' }
-            // ],
 const app = Vue.createApp({
     data() {
         return {
             goal: 'Finish the course and learn Vue!',
-            notGoal: 'You finished the course and learned Vue, you are a Master Vue!'
-            // vueLink: 'https://vuejs.org/'
+            notGoal: 'Master Vue and building amazing apps!',
+            vueLink: 'https://vuejs.org/'
         }
     },
     methods: {
@@ -28,3 +21,11 @@ const app = Vue.createApp({
 });
 
 app.mount('.user-goal');
+
+// items: [
+            //     { courseGoal: 'Finish the course and learn Vue!' },
+            //     { courseGoal: 'Finish the course of Kotlin!' },
+            //     { courseGoal: 'Finish the course of Modern Web!' },
+            //     { courseGoal: 'Finish the course of React!' },
+            //     { courseGoal: 'Finish the course of React Native!' }
+            // ],
