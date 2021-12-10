6554ad02f1bf91ea6a2a5bdb7ece03f781203883
taiprogrammer <taiza.marques@bandtec.com.br>
2021-12-10 15:24:46 -0300

    making first practice

---
+++ js/appThree.js
@@ -0,0 +1,34 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            name: 'Morgana',
+            age: 21,
+            ageInFiveYears: 26,
+            image: 'https://i.pinimg.com/originals/59/44/0b/59440b32af377c7c14f7234526a5f2f9.png',
+            enteredValue: ""
+        }
+    },
+    methods: {
+        showFavoriteNumber() {
+            const randomNumber = Math.random();
+            if (randomNumber >= 0 && randomNumber <= 5) {
+                return `${randomNumber.toFixed(2)}`
+            } else {
+                return 'This is not your favorite number'
+            }
+        },
+
+        changeName() {
+            this.name = this.enteredValue
+
+            if (this.image === 'https://i.pinimg.com/originals/59/44/0b/59440b32af377c7c14f7234526a5f2f9.png')  {
+                this.image = 'https://criticalhits.com.br/wp-content/uploads/2021/03/Lux_16-910x537.jpg'
+            } else {
+                this.image = 'https://i.pinimg.com/originals/59/44/0b/59440b32af377c7c14f7234526a5f2f9.png'
+            }
+            
+        }
+    }
+})
+
+app.mount(".my-info")
--- js/appTwo.js
+++ js/appTwo.js
@@ -1,31 +1,41 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            goal: 'Finish the course and learn Vue!',
-            notGoal: 'Master Vue and building amazing apps!',
-            vueLink: 'https://vuejs.org/'
-        }
+  data() {
+    return {
+      goal: "Finish the course and learn Vue!",
+      notGoal: "Master Vue and building amazing apps!",
+      vueLink: "https://vuejs.org/",
+    };
+  },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        console.log(randomNumber);
+        return this.goal;
+      } else {
+        console.log(randomNumber);
+        return this.notGoal;
+      }
     },
-    methods: {
-        outputGoal() {
-            const randomNumber = Math.random();
-            if (randomNumber < 0.5) {
-                console.log(randomNumber)
-                return this.goal
-            } else {
-                console.log(randomNumber)
-                return this.notGoal
-            }
-        }
-    }
+  },
 });
 
-app.mount('.user-goal');
+const appTwo = Vue.createApp({
+  data() {
+    return {
+      yourGoal: "You have to finish the course",
+    };
+  },
+});
+
+app.mount(".user-goal");
+
+appTwo.mount(".user-goal-two");
 
 // items: [
-            //     { courseGoal: 'Finish the course and learn Vue!' },
-            //     { courseGoal: 'Finish the course of Kotlin!' },
-            //     { courseGoal: 'Finish the course of Modern Web!' },
-            //     { courseGoal: 'Finish the course of React!' },
-            //     { courseGoal: 'Finish the course of React Native!' }
-            // ],
+//     { courseGoal: 'Finish the course and learn Vue!' },
+//     { courseGoal: 'Finish the course of Kotlin!' },
+//     { courseGoal: 'Finish the course of Modern Web!' },
+//     { courseGoal: 'Finish the course of React!' },
+//     { courseGoal: 'Finish the course of React Native!' }
+// ],
