90dec8a356f0973676465152bfd54856f85100e4
taiprogrammer <taiza.marques@bandtec.com.br>
2021-12-15 15:00:50 -0300

    dynamic style

---
+++ js/appNine.js
@@ -0,0 +1,33 @@
+const app = Vue.createApp({
+
+    data() {
+        return {
+            boxASelected: false,
+            boxBSelected: false,
+            boxCSelected: false,
+            highlited: 'border-color: blue',
+            notHighlited: 'border-color: #ccc'
+        }
+    },
+
+    methods: {
+        boxSelected(box) {
+            if (box === 'A') {
+                this.boxASelected = true
+                this.boxBSelected = false
+                this.boxCSelected = false
+            } else if (box === 'B') {
+                this.boxBSelected = true
+                this.boxASelected = false
+                this.boxCSelected = false
+            }
+            else if (box === 'C') {
+                this.boxCSelected = true
+                this.boxBSelected = false
+                this.boxASelected = false
+            }
+        }
+    }
+})
+
+app.mount("#styling")
