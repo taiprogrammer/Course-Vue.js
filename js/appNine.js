c55a371613b44d5f460f0aa9db1f3f2a8ed5276c
taiprogrammer <taiza.marques@bandtec.com.br>
2021-12-15 15:04:08 -0300

    dynamic style with vue

--- js/appNine.js
+++ js/appNine.js
@@ -1,33 +1,31 @@
 const app = Vue.createApp({
+  data() {
+    return {
+      boxASelected: false,
+      boxBSelected: false,
+      boxCSelected: false,
+      highlited: "blue",
+      notHighlited: "#ccc",
+    };
+  },
 
-    data() {
-        return {
-            boxASelected: false,
-            boxBSelected: false,
-            boxCSelected: false,
-            highlited: 'border-color: blue',
-            notHighlited: 'border-color: #ccc'
-        }
+  methods: {
+    boxSelected(box) {
+      if (box === "A") {
+        this.boxASelected = true;
+        this.boxBSelected = false;
+        this.boxCSelected = false;
+      } else if (box === "B") {
+        this.boxBSelected = true;
+        this.boxASelected = false;
+        this.boxCSelected = false;
+      } else if (box === "C") {
+        this.boxCSelected = true;
+        this.boxBSelected = false;
+        this.boxASelected = false;
+      }
     },
+  },
+});
 
-    methods: {
-        boxSelected(box) {
-            if (box === 'A') {
-                this.boxASelected = true
-                this.boxBSelected = false
-                this.boxCSelected = false
-            } else if (box === 'B') {
-                this.boxBSelected = true
-                this.boxASelected = false
-                this.boxCSelected = false
-            }
-            else if (box === 'C') {
-                this.boxCSelected = true
-                this.boxBSelected = false
-                this.boxASelected = false
-            }
-        }
-    }
-})
-
-app.mount("#styling")
+app.mount("#styling");
