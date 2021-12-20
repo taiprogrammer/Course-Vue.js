ed2372d4273898165179ccfbdc0de492db25d3aa
taiprogrammer <taiza.marques@bandtec.com.br>
2021-12-20 10:06:22 -0300

    refactoring project add films with v-if attribute

--- js/rendering-contents-contionally-list/filmsToWatch.js
+++ js/rendering-contents-contionally-list/filmsToWatch.js
@@ -1,9 +1,25 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            films: []
-        }
-    }
-})
+  data() {
+    return {
+      films: [],
+      enteredValue: "",
+    };
+  },
 
-app.mount("#films")
+  methods: {
+    addFilm() {
+      if (this.enteredValue === "") {
+        alert("You can't add an empty value");
+      } else {
+        this.films.push(this.enteredValue);
+        this.enteredValue = "";
+      }
+    },
+
+    resetList() {
+      this.films = [];
+    },
+  },
+});
+
+app.mount("#films");
