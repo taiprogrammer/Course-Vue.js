f687decd523437816b70f79842c477a6a15cf2d2
taiprogrammer <taiza.marques@sptech.school>
2021-12-22 15:50:16 -0300

    currency converter done

--- js/exercising/currencyConverter.js
+++ js/exercising/currencyConverter.js
@@ -10,34 +10,64 @@ const app = Vue.createApp({
       resultOne: "",
       resultTwo: "",
       resultThree: "",
+      resultFour: "",
+      resultFive: "",
+      resultSix: ""
     };
   },
 
   methods: {
     convertUsdToBrl() {
-      if (this.enteredValueUsd.length < 0) {
+      if (this.enteredValueUsd.length <= 0) {
         alert("Please enter a value");
       } else {
-        this.resultOne = (this.enteredValueUsd * 5.72).toFixed(2);
+        this.resultOne = 'R$ '.concat((this.enteredValueUsd * 5.72).toFixed(2));
       }
     },
 
     convertBrlToUsd() {
-        if (this.enteredValueBrl.length < 0) {
-            alert("Please enter a value");
-        }
-        else {
-            this.resultTwo = (this.enteredValueBrl / 5.72).toFixed(2)
-        }
+      if (this.enteredValueBrl.length <= 0) {
+        alert("Please enter a value");
+      } else {
+        this.resultTwo = '$ '.concat((this.enteredValueBrl / 5.72).toFixed(2));
+      }
     },
 
     convertCadToBrl() {
-        if (this.enteredValueCad < 0) {
-            alert("Please enter a value");
-        }
-        else {
-            this.resultThree = (this.enteredValueCad * 4.45).toFixed(2);
-        }
+      if (this.enteredValueCad <= 0) {
+        alert("Please enter a value");
+      } else {
+        this.resultThree = "R$ ".concat(
+          (this.enteredValueCad * 4.45).toFixed(2)
+        );
+      }
+    },
+
+    convertBrlToCad() {
+      if (this.enteredValueBrlToCad <= 0) {
+        alert('Please enter a value');
+      }
+      else {
+        this.resultFour = 'C$ '.concat((this.enteredValueBrlToCad / 4.45).toFixed(2))
+      }
+    },
+
+    convertEurToBrl() {
+      if (this.enteredValueEur <= 0) {
+        alert('Please enter a value');
+      }
+      else {
+        this.resultFive = 'R$ '.concat((this.enteredValueEur * 6.43).toFixed(2));
+      }
+    },
+
+    convertBrlToEur() {
+      if (this.enteredValueBrlToEur <= 0) {
+        alert('Please enter a value');
+      }
+      else {
+        this.resultSix = '€ '.concat((this.enteredValueBrlToEur / 6.43).toFixed(2))
+      }
     }
   },
 });
