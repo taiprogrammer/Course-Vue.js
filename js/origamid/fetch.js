478cc94f930dbae0788923de76db90063eabaac7
taiprogrammer <taiza.marques@sptech.school>
2022-03-25 18:23:29 -0300

    only true

--- js/origamid/fetch.js
+++ js/origamid/fetch.js
@@ -3,9 +3,12 @@ const app = Vue.createApp({
     return {
       apple: new Object(),
       google: new Object(),
+      fedido: false,
+      taiza: false,
     };
   },
   mounted() {
+    console.log(this.fedido);
     fetch("https://api.origamid.dev/stock/googl/quote")
       .then((response) => response.json())
       .then((google) => (this.google = google));
@@ -13,6 +16,22 @@ const app = Vue.createApp({
     fetch("https://api.origamid.dev/stock/aapl/quote")
       .then((response) => response.json())
       .then((apple) => (this.apple = apple));
+  },
+  methods: {
+    setSmell() {
+      if (!this.fedido) {
+        this.fedido = true;
+      } else {
+        this.fedido = false;
+      }
+    },
+    setSmellTaiza() {
+      if (!this.taiza) {
+        this.taiza = true;
+      } else {
+        this.taiza = false;
+      }
+    },
   },
 });
 
